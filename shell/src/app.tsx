ffbf01b94062de9dc93c3275f8a047f1bdcbd885
Jehyun <congmul@hotmail.com>
2022-05-15 13:54:46 -0700

    Update Shell environment.

--- shell/src/app.tsx
+++ shell/src/app.tsx
@@ -4,8 +4,7 @@ import * as React from 'react';
 
 const App = () => (
   <div>
-    <h1>Typescript</h1>
-    <h2>Remote 1 App</h2>
+    <h1>SHELL</h1>
     <React.Suspense fallback="Loading Button">
       {/* <RemoteButton /> */}
     </React.Suspense>
