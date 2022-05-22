e358edee6ea5c715404d2944c5cdd2ca3c18bec5
Jehyun <congmul@hotmail.com>
2022-05-22 07:50:46 -0700

    Update Nav icon

--- app/shell/src/components/Navbar/Navbar.tsx
+++ app/shell/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseChimneyWindow, faChartLine } from '@fortawesome/free-solid-svg-icons'
 import RemoteWrapper from '../RemoteWrapper/RemoteWrapper';
 
 const UserAvatar = RemoteWrapper(React.lazy(() => import('userManagement/UserAvatar')));
@@ -16,8 +17,8 @@ export default ():JSX.Element => {
             </div>
         </Link>
         <div className="link-wrapper">
-            <Link to="/home">Home</Link>
-            <Link to="/dashboard">Dash</Link>
+            <Link to="/home"><FontAwesomeIcon icon={faHouseChimneyWindow} /></Link>
+            <Link to="/dashboard"><FontAwesomeIcon icon={faChartLine} /></Link>
         </div>
         <div className="nav-bottom">
             <UserAvatar error={"Error"} />
